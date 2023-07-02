00b05f6580b336f01ae2a494b66353f1cf901c2d
Manny <manny@Zipcoders-MacBook-Pro-58.local>
2023-07-01 22:33:48 -0400

    completed bob and alice lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,24 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+
+        //create a scanner object with the name of userInput and initialize with system.in input
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Please enter your name: ");
+        String userName = userInput.nextLine();
+
+            while (!(userName.equals("Alice") || userName.equals("Bob"))) {
+
+                System.out.print("Please enter another name: ");
+                userName = userInput.nextLine();
+
+            }
+                    System.out.print("Greetings: " + userName );
 
     }
 }
